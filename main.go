da40e5b4979d8b1eaa9cb7d1ff5fd9694f5c0920
r_takaishi <ryo.takaishi.0@gmail.com>
2020-04-17 14:08:19 +0900

    stop to use urfave/cli

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/pkg/errors"
-	"github.com/urfave/cli"
 	"log"
 	"os"
 )
@@ -10,26 +9,22 @@ import (
 var Logger *log.Logger
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "vault-token-renewer"
-	app.Action = func(c *cli.Context) error {
-		tokenRenewer, err := NewRenewer(
-			os.Getenv("VAULT_ADDR"),
-			os.Getenv("VAULT_TOKEN"),
-			os.Getenv("VAULT_INCREMENT"),
-			os.Getenv("VAULT_GRACE_PERIOD"),
-		)
-		if err != nil {
-			return errors.Wrap(err, "Failed to create Renewer")
-		}
-
-		tokenRenewer.Run()
-
-		return nil
-	}
-
-	err := app.Run(os.Args)
+	err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run() error {
+	tokenRenewer, err := NewRenewer(
+		os.Getenv("VAULT_ADDR"),
+		os.Getenv("VAULT_TOKEN"),
+		os.Getenv("VAULT_INCREMENT"),
+		os.Getenv("VAULT_GRACE_PERIOD"),
+	)
+	if err != nil {
+		return errors.Wrap(err, "Failed to create Renewer")
+	}
+
+	return tokenRenewer.Run()
 }
