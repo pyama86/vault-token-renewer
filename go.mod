f3907cc637065e6023b76330a62197cb6bc2941e
r_takaishi <ryo.takaishi.0@gmail.com>
2020-04-17 16:06:30 +0900

    add /metrics endpoint with token ttl

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/hashicorp/vault/api v1.0.4
 	github.com/lestrrat-go/backoff v1.0.0
 	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v1.5.1
 )
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/hashicorp/vault/api"
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
 var Logger *log.Logger
+var tokenTTL = prometheus.NewGauge(prometheus.GaugeOpts{
+	Namespace: "vault_token_renewer",
+	Subsystem: "token",
+	Name:      "ttl",
+})
+
+func init() {
+	prometheus.MustRegister(tokenTTL)
+}
 
 func main() {
 	err := run()
@@ -26,5 +41,35 @@ func run() error {
 		return errors.Wrap(err, "Failed to create Renewer")
 	}
 
+	var httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	client, err := api.NewClient(&api.Config{Address: os.Getenv("VAULT_ADDR"), HttpClient: httpClient})
+	if err != nil {
+		return err
+	}
+	client.SetToken(os.Getenv("VAULT_TOKEN"))
+	go getTokenTTL(client)
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	go http.ListenAndServe(":8080", nil)
+
 	return tokenRenewer.Run()
+}
+
+func getTokenTTL(vault *api.Client) error {
+	for {
+		secret, err := vault.Auth().Token().Lookup(os.Getenv("VAULT_TOKEN"))
+		if err != nil {
+			return err
+		}
+		ttl, err := secret.Data["ttl"].(json.Number).Float64()
+		if err != nil {
+			return err
+		}
+		tokenTTL.Set(ttl)
+		time.Sleep(10 * time.Second)
+	}
+	return nil
 }
